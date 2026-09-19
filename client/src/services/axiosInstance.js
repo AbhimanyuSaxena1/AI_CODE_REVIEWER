cde16ab47d9b50ea911c4c57ca068c4db0c9270f
abhimanyusaxena1 <abhisaxena215.com>
2026-09-19 19:42:49 +0530

    added frontend env

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5173/api",
+    baseURL:import.meta.env.VITE_FRONTEND_URL+'/api',
 })
 
 export default axiosInstance;
